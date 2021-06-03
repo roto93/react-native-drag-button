c33d35fb441853ea495e185cacfb649deaa3c8d8
roto93 <roto93@yahoo.com.tw>
2021-06-03 13:16:49 +0800

    6/3 main feature complete

--- App.js
+++ App.js
@@ -19,30 +19,60 @@ export default function App() {
 
   const Handler = useAnimatedGestureHandler({
     onStart: () => { aniValue.value = 300 },
-    onActive: (evt) => {
-      let [X, Y] = [evt.x - size / 2, evt.y - size / 2]
+    onActive: (evt, ctx) => {
+      let [X, Y] = [evt.x - 300 / 2, evt.y - 300 / 2]
       let theta = 360 * Math.atan(Y / X) / (2 * Math.PI) //in deg
-      if (Y < 0) theta = 180 + (theta)
+      if (X > 0 & Y > 0) { }
+      else if (X < 0 & Y > 0) theta = 180 + (theta)
+      else if (X > 0 & Y < 0) { }
+      else if (X < 0 & Y < 0) theta = 180 + (theta)
+      theta += 90
       const checkAngle = (totalCase, caseId) => {
         //calculated in deg
-        let center = 360 * caseId / totalCase - 90
-        let upperLimit = center + 180 / totalCase
-        let lowerLimit = center - 180 / totalCase
-        if (theta < 30 & theta > 0) {
+        let center = 360 * caseId / totalCase
+        let upperLimit = (center + 180 / totalCase)
+        let lowerLimit = (center - 180 / totalCase)
+        console.log(center + '   ' + lowerLimit + '   ' + upperLimit + '   ' + theta)
+        if (theta < upperLimit & theta > lowerLimit) {
           console.log(caseId)
           return true
+        } else if (totalCase === caseId & theta > 0 & theta < 180 / totalCase) {
+          return true
         } else { return false }
+      }
 
-      }
       if (Y ** 2 > 70 ** 2 - X ** 2 & X ** 2 > 70 ** 2 - Y ** 2) {
-        if (checkAngle(10, 1)) runOnJS(setHoveredIndex)(true)
-        else { runOnJS(setHoveredIndex)(false) }
+        if (checkAngle(10, 1)) runOnJS(setHoveredIndex)(1)
+        else if (checkAngle(10, 2)) runOnJS(setHoveredIndex)(2)
+        else if (checkAngle(10, 3)) runOnJS(setHoveredIndex)(3)
+        else if (checkAngle(10, 4)) runOnJS(setHoveredIndex)(4)
+        else if (checkAngle(10, 5)) runOnJS(setHoveredIndex)(5)
+        else if (checkAngle(10, 6)) runOnJS(setHoveredIndex)(6)
+        else if (checkAngle(10, 7)) runOnJS(setHoveredIndex)(7)
+        else if (checkAngle(10, 8)) runOnJS(setHoveredIndex)(8)
+        else if (checkAngle(10, 9)) runOnJS(setHoveredIndex)(9)
+        else if (checkAngle(10, 10)) runOnJS(setHoveredIndex)(10)
+        else { runOnJS(setHoveredIndex)(0) }
       }
-      else { runOnJS(setHoveredIndex)(false) }
+      else { runOnJS(setHoveredIndex)(0) }
       aniValue.value = 300
     },
     onFinish: (evt) => {
-      runOnJS(setHoveredIndex)(false)
+      switch (hoveredIndex) {
+        case 1: runOnJS(alert)('1'); break
+        case 2: runOnJS(alert)('2'); break
+        case 3: runOnJS(alert)('3'); break
+        case 4: runOnJS(alert)('4'); break
+        case 5: runOnJS(alert)('5'); break
+        case 6: runOnJS(alert)('6'); break
+        case 7: runOnJS(alert)('7'); break
+        case 8: runOnJS(alert)('8'); break
+        case 9: runOnJS(alert)('9'); break
+        case 10: runOnJS(alert)('10'); break
+        default: runOnJS(alert)('0')
+      }
+
+      runOnJS(setHoveredIndex)(0)
       aniValue.value = 50
     }
   })
@@ -54,33 +84,30 @@ export default function App() {
     let x = Math.round(Math.cos(rad) * r)
     let y = Math.round(Math.sin(rad) * r)
     return (
-      <View style={{ opacity: hoveredIndex ? 0.2 : 1, position: 'absolute', width: 50, height: 50, borderRadius: 25, borderWidth: 1, transform: [{ translateX: x }, { translateY: y }] }}>
+      <View style={{ opacity: hoveredIndex == index ? 0.2 : 1, position: 'absolute', width: 50, height: 50, borderRadius: 25, borderWidth: 1, transform: [{ translateX: x }, { translateY: y }] }}>
 
       </View>
     )
   }
-
   return (
     <View style={styles.container}>
-      <PanGestureHandler onGestureEvent={Handler}  >
-        <Animated.View style={[aniStyle, { width: 50, height: 50, borderWidth: 0, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }]}>
-          <View style={{ width: 50, height: 50, borderWidth: 1 }} />
-          <Btn totalCount={10} index={1} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={2} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={3} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={4} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={5} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={6} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={7} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={8} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={9} hoveredIndex={hoveredIndex} />
-          <Btn totalCount={10} index={10} hoveredIndex={hoveredIndex} />
-
-
-
-
-        </Animated.View>
-      </PanGestureHandler>
+      <Animated.View style={[aniStyle, { borderWidth: 0, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }]}>
+        <PanGestureHandler onGestureEvent={Handler} >
+          <Animated.View style={{ width: 300, height: 300, justifyContent: 'center', alignItems: 'center', }}>
+            <View style={{ width: 50, height: 50, borderWidth: 1 }} />
+            <Btn totalCount={10} index={1} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={2} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={3} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={4} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={5} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={6} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={7} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={8} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={9} hoveredIndex={hoveredIndex} />
+            <Btn totalCount={10} index={10} hoveredIndex={hoveredIndex} />
+          </Animated.View>
+        </PanGestureHandler>
+      </Animated.View>
       <StatusBar style="auto" />
     </View >
   );
